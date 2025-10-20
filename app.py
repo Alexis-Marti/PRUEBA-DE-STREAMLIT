f36ce4ab36f685634a854f1b1bda1c93fa913e87
Alexis-Marti <00065821@uca.edu.sv>
2025-10-20 09:06:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from modulos.login import login
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 
 # Llamamos a la función mostrar_venta para mostrar el mensaje en la app
-mostrar_venta()
 login()
+mostrar_venta()
 
 
 
