91894af9024441024a0f8d65039e9f430f9a5e2c
Alexis-Marti <00065821@uca.edu.sv>
2025-10-20 14:36:37 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,20 @@ from modulos.venta import mostrar_venta  # Importa la función para mostrar vent
 
 # Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
-    # Si la sesión está iniciada, mostrar el contenido de ventas
-    mostrar_venta()
+      # Mostrar el menú lateral
+    opciones = ["Ventas", "Otra opción"]  # Agrega más opciones si las necesitas
+    seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+
+    # Según la opción seleccionada, mostramos el contenido correspondiente
+    if seleccion == "Ventas":
+        mostrar_venta()
+    elif seleccion == "Otra opción":
+        st.write("Has seleccionado otra opción.")  # Aquí podrías agregar el contenido de otras opciones
+
 else:
     # Si la sesión no está iniciada, mostrar el login
     login()
+
 
 
 
