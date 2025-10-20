9e76ad8ccbf65eb76065c3bbbc9932294eea5fc9
Alexis-Marti <00065821@uca.edu.sv>
2025-10-20 13:08:17 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from modulos.config.conexion import obtener_conexion
 
-
 def verificar_usuario(Usuario, Contra):
     con = obtener_conexion()
     if not con:
@@ -18,18 +17,27 @@ def verificar_usuario(Usuario, Contra):
         con.close()
 
 def login():
+    # Mostrar título y formulario de login
     st.title("Inicio de sesión")
     Usuario = st.text_input("Usuario", key="usuario_input")
-    Contra= st.text_input("Contraseña", type="password", key="contrasena_input")
+    Contra = st.text_input("Contraseña", type="password", key="contrasena_input")
 
+    # Si ya hay una sesión iniciada, mostrar un mensaje de bienvenida
+    if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
+        st.success(f"Sesión iniciada correctamente como {st.session_state['usuario']}")
+        return
+
+    # Si no hay sesión iniciada, mostrar el formulario de login
     if st.button("Iniciar sesión"):
         tipo = verificar_usuario(Usuario, Contra)
         if tipo:
             st.session_state["usuario"] = Usuario
-            st.session_state["tipo_usuario"] = Usuario
+            st.session_state["tipo_usuario"] = tipo
             st.success(f"Bienvenido ({Usuario})")
             st.session_state["sesion_iniciada"] = True
-            st.rerun()
+            st.rerun()  # Recargar la página para reflejar el estado de la sesión
         else:
             st.error("Credenciales incorrectas")
+
+
 
