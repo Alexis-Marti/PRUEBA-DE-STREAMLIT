6bb782cc91e05593b3f452c1d0c9abd862805e11
Alexis-Marti <00065821@uca.edu.sv>
2025-10-20 13:05:28 -0600

    Update login.py

--- modulos/login.py
+++ modulos/login.py
@@ -10,7 +10,7 @@ def verificar_usuario(Usuario, Contra):
 
     try:
         cursor = con.cursor()
-        query = "SELECT Usuario, Contra FROM Empleado WHERE Usuario = %s AND Contra = %s"
+        query = "SELECT Usuario, Contra FROM Empleados WHERE Usuario = %s AND Contra = %s"
         cursor.execute(query, (Usuario, Contra))
         result = cursor.fetchone()
         return result[0] if result else None
