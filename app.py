99fb23f0fb18bd807574b142e46a85748b1642cb
Alexis-Marti <00065821@uca.edu.sv>
2025-10-21 11:14:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ from modulos.login import login
 # Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
     # Mostrar el menú lateral
-opciones = ["Ventas", "Otra opción"]  # Agrega más opciones si las necesitas
-seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
+    opciones = ["Ventas", "Otra opción"]  # Agrega más opciones si las necesitas
+    seleccion = st.sidebar.selectbox("Selecciona una opción", opciones)
 
-# Según la opción seleccionada, mostramos el contenido correspondiente
-if seleccion == "Ventas":
-    mostrar_venta()
-elif seleccion == "Otra opción":
-    st.write("Has seleccionado otra opción.")  # Aquí podrías agregar el contenido de otras opciones
+    # Según la opción seleccionada, mostramos el contenido correspondiente
+    if seleccion == "Ventas":
+        mostrar_venta()
+    elif seleccion == "Otra opción":
+        st.write("Has seleccionado otra opción.")  # Aquí podrías agregar el contenido de otras opciones
 
 else:
     # Si la sesión no está iniciada, mostrar el login
     login()
+
+
 
 
 
